e279508ae1acfef33702144f333a0642e08409f9
Srikanth Roopa <Srikanth.Roopa1@T-Mobile.com>
2017-12-28 11:55:45 -0800

    Moved activity to different package.

--- app/src/main/java/com/srikanth/androidmvpkotlin/views/activities/MainActivity.kt
+++ app/src/main/java/com/srikanth/androidmvpkotlin/views/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.srikanth.androidmvpkotlin
+package com.srikanth.androidmvpkotlin.views.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.srikanth.androidmvpkotlin.R
 
 class MainActivity : AppCompatActivity() {
 
