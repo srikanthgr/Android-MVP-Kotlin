c2ad65764fd67aef4401fafda1dfa71786cfd9df
Srikanth Roopa <Srikanth.Roopa1@T-Mobile.com>
2018-01-08 12:25:45 -0800

    Adding TODO for the connection manager

--- app/src/main/java/com/srikanth/androidmvpkotlin/utils/ConnectionHelper.kt
+++ app/src/main/java/com/srikanth/androidmvpkotlin/utils/ConnectionHelper.kt
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager
 
 class ConnectionHelper(private val context: Context) {
 
+    //TODO add permissions for access network info
     fun isOnline(): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val netInfo = connectivityManager.activeNetworkInfo
