93bd24670814a0985c16f7107e94bd1a2d74105a
Srikanth Roopa <Srikanth.Roopa1@T-Mobile.com>
2018-01-08 12:20:21 -0800

    Included connection manager.

--- app/src/main/java/com/srikanth/androidmvpkotlin/utils/ConnectionHelper.kt
+++ app/src/main/java/com/srikanth/androidmvpkotlin/utils/ConnectionHelper.kt
@@ -1,5 +1,14 @@
 package com.srikanth.androidmvpkotlin.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
 
-class ConnectionHelper {
+
+class ConnectionHelper(private val context: Context) {
+
+    fun isOnline(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = connectivityManager.activeNetworkInfo
+        return netInfo != null && netInfo.isConnected
+    }
 }
